93f701236bf1a4f462ba7b24ffcd114bb55c0650
chai.jiawei <jiawei_chai@126.com>
2020-09-14 16:31:12 +0800

    个人中心

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('users', 'UsersController');

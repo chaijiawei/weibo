d570fa659a8cdb103460db0e2a08e413e578a066
chaijiawei <jiawei_chai@126.com>
2024-01-25 13:49:29 +0800

    微博发布

--- app/Http/Controllers/MicroBlogController.php
+++ app/Http/Controllers/MicroBlogController.php
@@ -7,9 +7,25 @@ use Illuminate\Http\Request;
 
 class MicroBlogController extends Controller
 {
+    public function __construct()
+    {
+       $this->middleware('auth')->only('store');
+    }
     public function index() {
         $microBlogs = MicroBlog::latest()->with('user')->paginate();
 
         return view('micro-blog.index', compact('microBlogs'));
+    }
+
+    public function store(Request $request) {
+        $data = $this->validate($request, [
+            'content' => 'required|string|min:1|max:255'
+        ], [], [
+            'content' => '微博内容'
+        ]);
+
+        $request->user()->microBlogs()->create($data);
+
+        return back()->with(['msg' => '微博发布成功']);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,15 @@
 @section('title', '首页')
 
 @section('content')
-  <h3>我是首页</h3>
+  <form action="{{ route('micro-blogs.store') }}" method="post">
+    @include('layouts.errors')
+    @csrf
+
+    <div class="mb-3">
+      <label for="content" class="form-label">请发表您的见解...</label>
+      <textarea class="form-control" name="content" id="content" rows="5">{{ old('content') }}</textarea>
+    </div>
+
+    <button type="submit" class="btn btn-primary">发布</button>
+  </form>
 @endsection
